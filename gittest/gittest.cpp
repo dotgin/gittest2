38c76adf741439a54feeab4957e2ef965e701f01
dotgin <dotgin@yandex.ru>
2015-08-29 21:11:10 +0300

    feature_1

--- gittest/gittest.cpp
+++ gittest/gittest.cpp
@@ -3,11 +3,18 @@
 
 #include "stdafx.h"
 #include <stdio.h>
+#include <iostream>
+
+
+std::string feature1Logic()
+{
+	return "feature_1";
+}
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf( "hello %d world", argc );
+	printf( "hello %d world %s", argc, feature1Logic().c_str() );
 	return 0;
 }
 
