bf93177488232f38befbb06373335445d6130f2b
dotgin <dotgin@yandex.ru>
2015-08-29 21:14:08 +0300

    fet2

--- gittest/gittest.cpp
+++ gittest/gittest.cpp
@@ -3,11 +3,18 @@
 
 #include "stdafx.h"
 #include <stdio.h>
+#include <iostream>
+
+
+std::string fet2()
+{
+	return "fet2";
+}
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf( "hello %d world", argc );
+	printf( "%s hello %d world", fet2().c_str(), argc );
 	return 0;
 }
 
