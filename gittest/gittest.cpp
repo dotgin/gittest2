fe9d82b654078b0ea1067bced9de8f919369680e
dotgin <dotgin@yandex.ru>
2015-08-29 20:53:22 +0300

    Init

---
+++ gittest/gittest.cpp
@@ -0,0 +1,13 @@
+// gittest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf( "hello %d world", argc );
+	return 0;
+}
+
